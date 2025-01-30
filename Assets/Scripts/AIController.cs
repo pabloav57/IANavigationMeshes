9357ab9282ea88a7c04ba0634486451bc951a8ea
pabloav57 <pabloav57@gmail.com>
2025-01-30 21:50:08 +0100

    Import S0602Solution

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -1,29 +1,29 @@
-using Unity.AI.Navigation.Editor;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AIController : MonoBehaviour
-{
+public class AIController : MonoBehaviour {
+
     public NavMeshAgent agent;
+    public GameObject target;
     Animator anim;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        agent = this.GetComponent<NavMeshAgent>();
-        anim = this.GetComponent<Animator>();
-        
+    void Start() {
+
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        //agent.speed = Random.Range(3.0f, 8.0f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(agent.remainingDistance < 2)
-        {
+    void Update() {
+
+        agent.SetDestination(target.transform.position);
+        if (agent.remainingDistance < 2.0f) {
+
             anim.SetBool("isMoving", false);
-        }
-        else
-        {
+        } else {
+
             anim.SetBool("isMoving", true);
         }
     }
--- Assets/Scripts/AgentManager.cs
+++ Assets/Scripts/AgentManager.cs
@@ -1,34 +1,37 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AgentManager : MonoBehaviour
-{
+public class AgentManager : MonoBehaviour {
+
+    public GameObject destination;
     List<NavMeshAgent> agents = new List<NavMeshAgent>();
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start() {
+
         GameObject[] a = GameObject.FindGameObjectsWithTag("AI");
-        foreach (GameObject go in a )
-        {
+        foreach (GameObject go in a) {
+
             agents.Add(go.GetComponent<NavMeshAgent>());
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
+
+    void Update() {
+
+        if (Input.GetMouseButtonDown(0)) {
+
             RaycastHit hit;
-            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
-            {
-                foreach(NavMeshAgent a in agents)
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+
+                Vector3 targetPosition = hit.point;
+                destination.transform.position = targetPosition;
+                foreach (NavMeshAgent a in agents) {
+
                     a.SetDestination(hit.point);
+                }
             }
         }
-
-        }
     }
-
+}
